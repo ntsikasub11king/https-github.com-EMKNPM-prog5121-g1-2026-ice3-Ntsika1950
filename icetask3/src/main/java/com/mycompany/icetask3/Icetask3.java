d74a18117f892b1a6effdc1bca91e7a3c7c1ec9d
ntsikasub11king <ntsikamazibuko645@gmail.com>
2026-04-24 09:37:44 +0200

    2nd part

--- icetask3/src/main/java/com/mycompany/icetask3/Icetask3.java
+++ icetask3/src/main/java/com/mycompany/icetask3/Icetask3.java
@@ -13,4 +13,13 @@ public class Icetask3 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-}
+
+    private String bloodType;
+    private String rhFactor;
+
+    public Icetask3() {
+        bloodType = "O";
+        rhFactor = "+";
+        }
+
+  
